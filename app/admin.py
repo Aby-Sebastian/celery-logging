939743afcfbf81b9c784307f67644deda199e12f
Aby Sebastian <abysebastian98@gmail.com>
2023-01-28 19:13:01 +0530

    Created DB and registerd in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Celery_log
 
 # Register your models here.
+
+class CeleryLogAdmin(admin.ModelAdmin):
+    list_display = ["task_id", "status", "arguments"]
+
+admin.site.register(Celery_log, CeleryLogAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+'''
+Data to log:
+● celery task ID
+● status of the celery task (Success, failed, pending, running, etc)
+● arguments used to run the job (ie: filename and the data count))
+'''
+
+class Celery_log(models.Model):
+    task_id = models.UUIDField(editable=False, unique=True)
+    status = models.CharField(max_length=20)
+    arguments = models.JSONField(max_length=50)
+
+    class Meta:
+        ordering = ["-id"]
+
+    def __str__(self) -> str:
+        return str(self.arguments)
